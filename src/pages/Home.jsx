6decf02b658660701fa8d7ed32e913d9ef9d879a
ANAIF1 <anaifeshetu21@gamil.com>
2026-07-28 11:53:23 +0300

    minor change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ import {
 import Header from "../componenet/Header";
 import Herosection from "../componenet/Herosection";
 import OurServices from "./Ourservice";
-import BookingPage from "./BookingPage";
+import BookingPage from "./Bookingpage";
 import ContactSection from "./Contactsection";
 import WhyChooseEthiomassage from "../pages/Whychoseethiomassage";
 
